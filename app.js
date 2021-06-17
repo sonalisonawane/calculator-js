6163c05e805bdff53cb8ad9e69f7aa4bc23df65e
sonali sonawane <sonawanesonali41@gmail.com>
2021-06-17 16:26:36 +0530

    calculator functionality added

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-inputBtns = document.querySelectorAll(".btn")
+** *
+let inputBtns = document.querySelectorAll(".btn")
+let resultBox = document.querySelector(".result-box")
+let screenValue = "" //value inside result box
 
 inputBtns.forEach(element => {
-    element.addEventListener("click", function() {
-        i
+    element.addEventListener("click", function(e) {
+        let buttonText = e.target.value;
+        if (buttonText == "x") {
+            buttonText = "*";
+            screenValue += buttonText;
+            resultBox.value = screenValue;
+        } else if (buttonText == "RESET") {
+            screenValue = ""
+            resultBox.value = screenValue;
+        } else if (buttonText == "DEL") {
+            // screenValue -= 1;
+            resultBox.value = resultBox.value.substring(0, resultBox.value.length - 1);
+        } else if (buttonText == "=") {
+            resultBox.value = eval(screenValue)
+        } else {
+            screenValue += buttonText;
+            resultBox.value = screenValue;
+
+        }
+    })
+    window.addEventListener("keydown", function(e) {
+        newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        if (e.key == "=" || "ENTER") {
+            resultBox.value = eval(screenValue)
+        } else if (e.key == "Backspace") {
+            resultBox.value = resultBox.value.substring(0, resultBox.value.length - 1);
+        }
+
+
     })
 });
